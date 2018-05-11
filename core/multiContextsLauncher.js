c5bca2af41f7e636bd0e57e75344aa34afe90344
Acer 2 <sava.dumitru.daniel@gmail.com>
2018-05-11 10:19:57 +0300

    categorize tests

---
+++ core/multiContextsLauncher.js
@@ -0,0 +1,24 @@
+var path = require("path");
+process.env.PRIVATESKY_TMP = path.normalize(__dirname + "/../../../tmp");
+require("../../engine/launcher");//.core.enableTesting();
+require("../../libraries/launcher/debugFacilitator").debugForks(true);
+//require("../../engine/core").enableTesting();
+var assert = require('double-check').assert;
+$$.requireLibrary("testSwarms");
+
+function runCode(callback){
+    $$.swarm.start("testSwarms.multiContextsSwarm").begin(1,2,callback);
+    setTimeout(function () {
+        process.exit();
+    },1000);
+}
+
+$$.container.declareDependency("onlyNowICanRunThis", [$$.DI_components.swarmIsReady], function(fail, ready){
+    //console.log("onlyNowICanRunThis", fail, ready);
+    if(!fail){
+        assert.callback("MultiContexts test",function(callback){
+            runCode(callback);
+        },2000)
+
+    }
+});
---
+++ core/swarmBasicLauncher.js
@@ -0,0 +1,24 @@
+var path = require("path");
+process.env.PRIVATESKY_TMP = path.normalize(__dirname + "/../../../tmp");
+require("../../engine/launcher");//.core.enableTesting();
+require("../../libraries/launcher/debugFacilitator").debugForks(true);
+var assert=require("double-check").assert;
+
+$$.requireLibrary("testSwarms");
+
+function runCode(callback){
+    $$.swarm.start("testSwarms.simpleSwarm").begin(1,2,callback);
+    setTimeout(function () {
+        process.exit();
+    },1000);
+}
+
+$$.container.declareDependency("onlyNowICanRunThis", [$$.DI_components.swarmIsReady], function(fail, ready){
+    //console.log("onlyNowICanRunThis", fail, ready);
+    if(!fail){
+        assert.callback("Basic Swarm Test",function (callback) {
+            runCode(callback);
+        })
+
+    }
+});
