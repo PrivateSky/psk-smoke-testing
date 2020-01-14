bd9455db71352716d4abe455e7502afb7bdc9838
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-14 15:24:08 +0200

    Added test to combine existing constitution and newly created one by TIR

---
+++ basicUsingExistingAndInMemoryConstitution.js
@@ -0,0 +1,34 @@
+const path = require('path');
+require('../../psknode/bundles/pskruntime');
+require('../../psknode/bundles/virtualMQ');
+
+const tir = require("../../psknode/tests/util/tir.js");
+const assert = require('../../modules/double-check').assert;
+
+const domain = "local";
+
+assert.callback("Basic Test", (finished) => {
+    const constitutionPath = path.resolve(path.join(__dirname, '../../libraries/basicTestSwarms'));
+    const localDomain = tir.addDomain(domain, ["system", "specialAgent"], constitutionPath);
+
+    localDomain.swarms.describe("superEcho", {
+        superSay: function (message) {
+            this.return(null, 'SuperEcho ' + message);
+        }
+    });
+
+    tir.launch(6000, () => {
+        $$.interactions.startSwarmAs(`${domain}/agent/system`, "echo", "say", "Hello")
+            .onReturn((err, result) => {
+                assert.equal("Echo Hello", result);
+
+                $$.interactions.startSwarmAs(`${domain}/agent/specialAgent`, "superEcho", "superSay", "Hello")
+                    .onReturn((err, result) => {
+                        assert.equal("SuperEcho Hello", result);
+
+                        finished();
+                        tir.tearDown(0);
+                    })
+            })
+    });
+}, 6000);
