799e003e7768b3f087a222c589ae4ef8e030645b
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-14 15:15:25 +0200

    Reverted test

--- basic.js
+++ basic.js
@@ -6,23 +6,22 @@ const assert = require('../../modules/double-check').assert;
 
 const domain = "local";
 
-// assert.callback("Basic Test", (finished) => {
-//     const localDomain = tir.addDomain(domain, ["system", "specialAgent"]);
-//
-//     localDomain.swarms.describe("echo", {
-//         say: function (message) {
-//             this.return(null, 'Echo ' + message);
-//         }
-//     });
-//
-//     tir.launch(6000, () => {
-//         $$.interactions.startSwarmAs(`${domain}/agent/system`, "echo", "say", "Hello")
-//             .onReturn((err, result) => {
-//                  assert.equal("Echo Hello", result);
-//                  finished();
-//                  tir.tearDown(0);
-//             })
-//     });
-// }, 6000);
+assert.callback("Basic Test", (finished) => {
+    const localDomain = tir.addDomain(domain, ["system", "specialAgent"]);
 
-tir.createConstitutionFromSources(['/home/stefancosmin/psk/privatesky-swarm-engine/libraries/superSwarm', '/home/stefancosmin/psk/privatesky-swarm-engine/libraries/basicTestSwarms'], '/tmp/tir-KjfVkr');
+    localDomain.swarms.describe("echo", {
+        say: function (message) {
+            this.return(null, 'Echo ' + message);
+        }
+    });
+
+    tir.launch(6000, () => {
+        $$.interactions.startSwarmAs(`${domain}/agent/system`, "echo", "say", "Hello")
+            .onReturn((err, result) => {
+                 assert.equal("Echo Hello", result);
+                 finished();
+                 tir.tearDown(0);
+            })
+    });
+}, 6000);
+
