394b5aac8b97539c424581d98e45a6091e877368
asac <asac.cool@gmail.com>
2020-02-19 15:26:59 +0200

    edfs tests refactoring

--- edfs/basic.js
+++ edfs/basic.js
@@ -1,58 +1,56 @@
-const DOMAIN_CONSTITUTION_FOLDER = "./domain";
+const DOMAIN_CONSTITUTION_FOLDER = "./testConstitution";
 require("../../../psknode/bundles/testsRuntime");
 
 const tir = require("../../../psknode/tests/util/tir");
 const assert = require("double-check").assert;
 
-assert.begin("basic test for edfs", function(){
-    //... do cleaning if necessary
-}, 5000);
+assert.callback("Test EDFS capabilities", (testFinishCallback) => {
+    function prepareCSB(endpoint, callback) {
+        const EDFS = require("edfs");
+        let edfs = EDFS.attachToEndpoint(endpoint);
+        let bar = edfs.createBar();
 
-function prepareCSB(endpoint, callback){
-    const EDFS = require("edfs");
-    let edfs = EDFS.attachToEndpoint(endpoint);
-    let bar = edfs.createBar();
+        bar.addFolder("../../../psknode/bundles", EDFS.constants.CSB.CONSTITUTION_FOLDER, (err) => {
+            if (err) {
+                throw err;
+            }
 
-    bar.addFolder("../../../psknode/bundles", EDFS.constants.CSB.CONSTITUTION_FOLDER, (err)=>{
-        if(err){
+            //bar.addFile...
+            tir.buildConstitution(DOMAIN_CONSTITUTION_FOLDER, bar, (err) => {
+                if (err) {
+                    throw err;
+                }
+                callback(undefined, bar.getSeed().toString());
+            });
+        });
+    }
+
+    function loadCSBAndStartTesting(err, seed) {
+        if (err) {
             throw err;
         }
 
-        //bar.addFile...
-
-        tir.buildConstitution(DOMAIN_CONSTITUTION_FOLDER, bar, (err)=>{
-            if(err){
-                throw err;
-            }
-            callback(undefined, bar.getSeed());
-        });
-    });
-}
-
-function loadCSBAndStartTesting(err, seed){
-    if(err){
-        throw err;
-    }
-    assert.callback("Test EDFS capabilities", (testFinishCallback)=>{
         const dossier = require("dossier");
-        dossier.load(seed, (err, csb)=>{
-            if(err){
+        dossier.load(seed, "ceva", (err, csb) => {
+            if (err) {
                 throw err;
             }
-            csb.startTransaction("transactionName", "phase").onReturn((err, res)=>{
+
+            csb.startTransaction("echo", "say").onReturn((err, res) => {
                 //... do asserts
                 //assert.isNull(err, "swarm finished with errors");
                 testFinishCallback();
                 tir.tearDown();
             });
         });
-    }, 10000);
-}
-
-tir.launchVirtualMQNode(function(err, port){
-    if(err){
-        throw err;
     }
-    const EDFS_HOST = `http://localhost:${port}`;
-    prepareCSB(EDFS_HOST, loadCSBAndStartTesting);
-});
+
+    tir.launchVirtualMQNode(function (err, port) {
+        if (err) {
+            throw err;
+        }
+        const EDFS_HOST = `http://localhost:${port}`;
+        prepareCSB(EDFS_HOST, loadCSBAndStartTesting);
+    });
+
+}, 10000);
---
+++ edfs/testConstitution/swarms/index.js
@@ -0,0 +1,6 @@
+$$.swarms.describe("echo",{
+    say: function(input){
+        this.return("Echo "+ input);
+    }
+});
+console.log("Loading swarm echo description");
