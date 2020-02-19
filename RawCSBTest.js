ecc0a0ef3b81a9026fda8390b82830b3007172ae
skutner <sava.dumitru.daniel@gmail.com>
2020-02-19 16:11:03 +0200

    refactored RawCSBTest.js

--- RawCSBTest.js
+++ RawCSBTest.js
@@ -1,18 +1,15 @@
-require("../../psknode/bundles/pskruntime");
-require("../../psknode/bundles/psknode");
-require("../../psknode/bundles/consoleTools");
+require("../../psknode/bundles/testsRuntime");
 require("../../psknode/bundles/virtualMQ");
 require("../../psknode/bundles/edfsBar");
+require("callflow");
 
 const path = require("path");
-const fs = require("fs");
-const brickStorageStrategyName = "http";
 $$.securityContext = require("psk-security-context").createSecurityContext();
 const edfsModule = require("edfs");
-const edfs = edfsModule.attach(brickStorageStrategyName);
-$$.brickTransportStrategiesRegistry.add(brickStorageStrategyName, edfsModule.createHTTPBrickTransportStrategy("http://127.0.0.1:9097"));
-
-const double_check = require("../../modules/double-check");
+const HTTPBrickTransportStrategy = edfsModule.HTTPBrickTransportStrategy;
+const brickTransportStrategyName = HTTPBrickTransportStrategy.prototype.HTTP_BRICK_TRANSPORT_STRATEGY;
+const edfs = edfsModule.attach(brickTransportStrategyName);
+const double_check = require("double-check");
 const assert = double_check.assert;
 const VirtualMQ = require("virtualmq");
 
@@ -32,38 +29,34 @@ function createServer(port, tempFolder, callback) {
             console.log("Server ready and available on port ", port);
             let url = `http://127.0.0.1:${port}`;
             callback(undefined, server, url);
-        }
+        }``
     });
 }
 
 double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
     assert.true(err === null || typeof err === "undefined", "Failed to create test folder");
 
-    const filePath = path.join(testFolder, "a.txt");
     const fileData = "ana are mere";
     assert.callback("TestRawCSB", (callback) => {
         $$.securityContext.generateIdentity((err, agentId) => {
             assert.true(err === null || typeof err === "undefined", "Failed to generate identity from security context.");
 
-            createServer(9097, "tmp", (err, server) => {
+            createServer(9097, path.join(testFolder,"tmp"), (err, server, url) => {
                 assert.true(err === null || typeof err === "undefined", "Failed to create server");
+                $$.brickTransportStrategiesRegistry.add(brickTransportStrategyName, new HTTPBrickTransportStrategy(url));
 
                 edfs.createCSB((err, rawCSB) => {
                     if (err) {
                         throw err;
                     }
+                    assert.true(err === null || typeof err === "undefined", "Failed to create CSB");
 
                     rawCSB.writeFile("a.txt", fileData, (err, barMapDigest) => {
-                        if (err) {
-                            throw err;
-                        }
                         assert.true(err === null || typeof err === "undefined", "Failed to write file in CSB");
                         assert.true(barMapDigest !== null && typeof barMapDigest !== "undefined", "Bar map digest is null or undefined");
 
                         edfs.loadCSB(rawCSB.getSeed(), (err, newRawCSB) => {
-                            if (err) {
-                                throw err;
-                            }
+                            assert.true(err === null || typeof err === "undefined", "Failed to load CSB.");
 
                             newRawCSB.readFile("a.txt", (err, data) => {
                                 assert.true(err === null || typeof err === "undefined", "Failed read file from CSB.");
@@ -80,7 +73,7 @@ double_check.createTestFolder("bar_test_folder", (err, testFolder) => {
                 });
             });
         });
-    }, 2000);
+    }, 3000);
 });
 
 
