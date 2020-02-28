e1d3de1dabe836cb205e09182f864dedf79629b3
asaccool <asac.cool@gmail.com>
2020-02-28 17:50:45 +0200

    test updates

--- core/serialCall.js
+++ core/serialCall.js
@@ -9,7 +9,7 @@ const agentTwo = 'secondAgent';
 const agents = [agentOne, agentTwo];
 
 const swarm = {
-	contoller: {
+	controller: {
 		public: {
 			resultValue: 'array',
 		},
@@ -73,15 +73,14 @@ assert.callback(
 	finished => {
 		tir.addDomain(domain, agents, swarm);
 
-		tir.launch(5000, () => {
-			tir.interact(domain, agentOne)
-				.startSwarm('contoller', 'serialExec', agentOne)
+		tir.launch(15000, () => {
+			$$.interactions.startSwarmAs(`${domain}/agent/${agentOne}`, "controller", "serialExec", agentOne)
 				.onReturn(result => {
 					assert.true(result.join('') === 'abc');
+					finished();
+					tir.tearDown(0);
 				});
 		});
-		finished();
-		tir.tearDown(0);
 	},
-	3500
+	13500
 );
