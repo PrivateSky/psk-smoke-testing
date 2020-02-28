afd2585c60aaceaeae281ca606d72c72e49dea6d
asaccool <asac.cool@gmail.com>
2020-02-28 14:03:51 +0200

    dossier test update

--- dossiers/leafletDossierType/swarms.js
+++ dossiers/leafletDossierType/swarms.js
@@ -1,10 +1,2 @@
-$$.swarms.describe("transactionHandler",{
-    start: function(identity, transactionName, methodName, ...args){
-        let transaction = $$.blockchain.startTransactionAs(identity, transactionName, methodName, ...args);
-        transaction.onReturn((err, result)=>{
-            this.return(err, result);
-        });
-    }
-});
 
 console.log("Loading swarm types");
