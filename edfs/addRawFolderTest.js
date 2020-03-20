41d372e370beb965f01a70c6c4efda59efc4387e
skutner <sava.dumitru.daniel@gmail.com>
2020-03-20 08:51:23 +0200

    added an options parameter in addFolder and addFile functions

--- edfs/addRawFolderTest.js
+++ edfs/addRawFolderTest.js
@@ -58,7 +58,7 @@ $$.flows.describe("AddRawFolder", {
     },
 
     addFolder: function (fsFolderPath, barPath, callback) {
-        this.bar.addFolder(fsFolderPath, barPath, false, (err, mapDigest) => {
+        this.bar.addFolder(fsFolderPath, barPath, {isEncrypted: false}, (err, mapDigest) => {
             if (err) {
                 return callback(err);
             }
