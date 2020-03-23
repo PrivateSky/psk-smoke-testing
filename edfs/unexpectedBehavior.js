60a4b0203b66f6323892c7a442d413039eb5d446
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-03-23 14:29:50 +0200

    unexpected behavior bar test

---
+++ edfs/unexpectedBehavior.js
@@ -0,0 +1,25 @@
+require("../../../psknode/bundles/testsRuntime");
+require("../../../psknode/bundles/pskruntime");
+
+const tir = require("../../../psknode/tests/util/tir");
+const assert = require("double-check").assert;
+
+assert.callback("We should be able to get a seed of a bar before finish writing?", (testFinishCallback) => {
+	tir.launchVirtualMQNode(function (err, port) {
+		if (err) {
+			throw err;
+		}
+		const EDFS_HOST = `http://localhost:${port}`;
+
+		const EDFS = require("edfs");
+		let edfs = EDFS.attachToEndpoint(EDFS_HOST);
+
+		let bar = edfs.createBar();
+		console.log("Early seed read", bar.getSeed());
+		bar.writeFile("just_a_path", "some_content", function(err){
+			assert.isNull(err);
+
+			testFinishCallback();
+		});
+	});
+}, 5000);
