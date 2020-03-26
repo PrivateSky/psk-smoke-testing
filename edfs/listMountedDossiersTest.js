39a5e2b8759fbbb1f57549c6fd7d1d6309a75958
skutner <sava.dumitru.daniel@gmail.com>
2020-03-26 10:58:31 +0200

    implemented listMountedDossiers API

---
+++ edfs/listMountedDossiersTest.js
@@ -0,0 +1,59 @@
+require("../../../psknode/bundles/testsRuntime");
+require("../../../psknode/bundles/pskruntime");
+
+const tir = require("../../../psknode/tests/util/tir");
+const assert = require("double-check").assert;
+
+assert.callback("List mounted dossiers test", (testFinishCallback) => {
+    tir.launchVirtualMQNode(function (err, port) {
+        if (err) {
+            throw err;
+        }
+        const EDFS_HOST = `http://localhost:${port}`;
+
+        const EDFS = require("edfs");
+        let edfs = EDFS.attachToEndpoint(EDFS_HOST);
+        const mountingPoints = [{path: "/", name: "dir", seed: edfs.createCSB().getSeed()}, {
+            path: "/dir",
+            name: "test",
+            seed: edfs.createCSB().getSeed()
+        }, {path: "/test", name: "folder", seed: edfs.createCSB().getSeed()}];
+
+        let raw_dossier = edfs.createCSB();
+        raw_dossier.writeFile("testFile", "testContent", (err) => {
+            if (err) {
+                throw err;
+            }
+
+            __mount(0);
+        });
+
+
+        function __mount(index) {
+            let mount = mountingPoints[index];
+            raw_dossier.mount(mount.path, mount.name, mount.seed, (err) => {
+                if (err) {
+                    throw err;
+                }
+
+                if (index === mountingPoints.length - 1) {
+                    startTest();
+                } else {
+                    __mount(index + 1);
+                }
+            });
+        }
+
+        function startTest() {
+            raw_dossier.listMountedDossiers("/", (err, mountedDossiers) => {
+                if (err) {
+                    throw err;
+                }
+
+                assert.true(mountedDossiers.length === mountingPoints.length);
+                testFinishCallback();
+            });
+        }
+    });
+}, 5000);
+
