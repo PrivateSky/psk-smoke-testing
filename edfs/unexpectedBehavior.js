fb4ea92ab9b5eeea5fbceea54bcc06284941bf21
skutner <sava.dumitru.daniel@gmail.com>
2020-03-30 13:20:10 +0300

    removed cloneTest.js and barHistoryStorageTest.js refactored unexpectedBehavior.js and unexpectedBehavior2.js

--- edfs/unexpectedBehavior.js
+++ edfs/unexpectedBehavior.js
@@ -5,21 +5,21 @@ const tir = require("../../../psknode/tests/util/tir");
 const assert = require("double-check").assert;
 
 assert.callback("We should be able to get a seed of a bar before finish writing?", (testFinishCallback) => {
-	tir.launchVirtualMQNode(function (err, port) {
-		if (err) {
-			throw err;
-		}
-		const EDFS_HOST = `http://localhost:${port}`;
+    tir.launchVirtualMQNode(function (err, port) {
+        if (err) {
+            throw err;
+        }
+        const EDFS_HOST = `http://localhost:${port}`;
 
-		const EDFS = require("edfs");
-		let edfs = EDFS.attachToEndpoint(EDFS_HOST);
+        const EDFS = require("edfs");
+        let edfs = EDFS.attachToEndpoint(EDFS_HOST);
 
-		let bar = edfs.createBar();
-		console.log("Early seed read", bar.getSeed());
-		bar.writeFile("just_a_path", "some_content", function(err){
-			assert.isNull(err);
+        let bar = edfs.createBar();
+        console.log("Early seed read", bar.getSeed());
+        bar.writeFile("just_a_path", "some_content", function (err) {
+            assert.true(typeof err === "undefined");
 
-			testFinishCallback();
-		});
-	});
+            testFinishCallback();
+        });
+    });
 }, 5000);
--- edfs/unexpectedBehavior2.js
+++ edfs/unexpectedBehavior2.js
@@ -5,29 +5,37 @@ const tir = require("../../../psknode/tests/util/tir");
 const assert = require("double-check").assert;
 
 assert.callback("We should be able to get a seed of a bar before finish writing?", (testFinishCallback) => {
-	tir.launchVirtualMQNode(function (err, port) {
-		if (err) {
-			throw err;
-		}
-		const EDFS_HOST = `http://localhost:${port}`;
+    tir.launchVirtualMQNode(function (err, port) {
+        if (err) {
+            throw err;
+        }
+        const EDFS_HOST = `http://localhost:${port}`;
 
-		const EDFS = require("edfs");
-		let edfs = EDFS.attachToEndpoint(EDFS_HOST);
-		let ref = edfs.createCSB();
-		ref.writeFile("simplefile", "withcontent", 0,()=>{
-			let raw_dossier = edfs.createCSB();
-			raw_dossier.mount("/", "test", ref.getSeed(), (err)=>{
-				assert.true(typeof err === "undefined");
-				raw_dossier.writeFile("just_a_path", "some_content", 0,function(err){
-					assert.true(typeof err === "undefined");
-					let raw_dossier_reloaded = edfs.loadRawDossier(raw_dossier.getSeed());
-					raw_dossier_reloaded.listFiles("/test", (err, files)=>{
-						assert.true(typeof err === "undefined");
-						assert.true(files.length==1);
-						testFinishCallback();
-					});
-				});
-			});
-		});
-	});
+        const EDFS = require("edfs");
+        let edfs = EDFS.attachToEndpoint(EDFS_HOST);
+        let ref = edfs.createCSB();
+        ref.writeFile("simplefile", "withcontent", 0, () => {
+            let raw_dossier = edfs.createCSB();
+            raw_dossier.mount("/", "test", ref.getSeed(), (err) => {
+                if (err) {
+                    throw err;
+                }
+                raw_dossier.writeFile("just_a_path", "some_content", 0, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    assert.true(typeof err === "undefined");
+                    let raw_dossier_reloaded = edfs.loadRawDossier(raw_dossier.getSeed());
+                    raw_dossier_reloaded.listFiles("/test", (err, files) => {
+                        if (err) {
+                            throw err;
+                        }
+                        assert.true(typeof err === "undefined");
+                        assert.true(files.length == 1);
+                        testFinishCallback();
+                    });
+                });
+            });
+        });
+    });
 }, 5000);
