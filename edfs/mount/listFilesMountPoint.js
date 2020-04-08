20d3eb1cc42b68988141083f4ce13955fccd2935
skutner <sava.dumitru.daniel@gmail.com>
2020-04-08 13:44:42 +0300

    A raw dossier cannot be mounted in a non-empty folder

--- edfs/mount/listFilesMountPoint.js
+++ edfs/mount/listFilesMountPoint.js
@@ -15,19 +15,23 @@ assert.callback("Test list files from a mount point", (testFinishCallback) => {
 		let edfs = EDFS.attachToEndpoint(EDFS_HOST);
 		let ref = edfs.createRawDossier();
 		const fileName = 'simpleFile';
-		ref.writeFile(fileName, "withcontent", 0, () => {
+		ref.writeFile(fileName, "withcontent", (err) => {
+			if (err) {
+				throw err;
+			}
+
 			let raw_dossier = edfs.createRawDossier();
-			raw_dossier.mount("/", "test", ref.getSeed(), (err) => {
+			raw_dossier.mount("/code", "test", ref.getSeed(), (err) => {
 				if (err) {
 					throw err;
 				}
-				raw_dossier.writeFile("just_a_path", "some_content", 0, function (err) {
+				raw_dossier.writeFile("just_a_path", "some_content", function (err) {
 					if (err) {
 						throw err;
 					}
 					assert.true(typeof err === "undefined");
 					let raw_dossier_reloaded = edfs.loadRawDossier(raw_dossier.getSeed());
-					raw_dossier_reloaded.listFiles("/test", (err, files) => {
+					raw_dossier_reloaded.listFiles("/code/test", (err, files) => {
 						if (err) {
 							throw err;
 						}
---
+++ edfs/mount/tryingToMountInANonEmptyFolder.js
@@ -0,0 +1,36 @@
+require("../../../../psknode/bundles/testsRuntime");
+require("../../../../psknode/bundles/pskruntime");
+
+const tir = require("../../../../psknode/tests/util/tir");
+const assert = require("double-check").assert;
+
+assert.callback("Test list files from a mount point", (testFinishCallback) => {
+    tir.launchVirtualMQNode(function (err, port) {
+        if (err) {
+            throw err;
+        }
+        const EDFS_HOST = `http://localhost:${port}`;
+
+        const EDFS = require("edfs");
+        let edfs = EDFS.attachToEndpoint(EDFS_HOST);
+        let ref = edfs.createRawDossier();
+        const fileName = 'simpleFile';
+        const folderName = "dir";
+        ref.writeFile(fileName, "withcontent", (err) => {
+            if (err) {
+                throw err;
+            }
+            let raw_dossier = edfs.createRawDossier();
+            raw_dossier.writeFile(folderName + "/randomPath", "content", (err)=>{
+                if (err) {
+                    throw err;
+                }
+                raw_dossier.mount(folderName, "test", ref.getSeed(), (err) => {
+                    assert.true(err && err.message === "Tried to mount in a non-empty folder");
+                    testFinishCallback();
+                });
+            });
+
+        });
+    });
+}, 5000);
