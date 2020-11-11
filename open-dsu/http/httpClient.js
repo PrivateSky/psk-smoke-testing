c86f1c6cd5669b21af520a034b6ca1913b60c535
bmastahac <bogdan.mastahac@gmail.com>
2020-11-11 11:15:56 +0200

    Updated open-dsu http client test.

--- open-dsu/http/httpClient.js
+++ open-dsu/http/httpClient.js
@@ -11,17 +11,17 @@ assert.callback('HTTP test', (callback) => {
         if (err) {
             return;
         }
-
+        const domain = 'default';
         assert.true(typeof http.poll === 'function');
         assert.true(typeof http.fetch === 'function');
         assert.true(typeof http.doPost === 'function');
         assert.true(typeof http.doPut === 'function');
 
-        http.doPut(`http://localhost:${port}/bricks/put-brick`, { test: 'da' }, (err, response) => {
+        http.doPut(`http://localhost:${port}/bricks/put-brick/${domain}`, { test: 'da' }, (err, response) => {
             const brickHash = JSON.parse(response).message
             assert.true(brickHash === '0de3c7406ae0dab1ed5f90fe20dd59992a7d6f9a383ff397b2fa9325c34011cf')
 
-            http.fetch(`http://localhost:${port}/bricks/get-brick/${brickHash}`).then((responseGetBrick) => {
+            http.fetch(`http://localhost:${port}/bricks/get-brick/${brickHash}/${domain}`).then((responseGetBrick) => {
 
                 assert.true(typeof responseGetBrick.text === 'function');
                 assert.true(typeof responseGetBrick.json === 'function');
@@ -32,7 +32,7 @@ assert.callback('HTTP test', (callback) => {
                 assert.true(typeof responseGetBrick.ok === 'boolean');
                 assert.true(responseGetBrick.ok === true);
 
-                http.fetch(`http://localhost:${port}/bricks/downloadMultipleBricks?hashes=${brickHash}`).then((responseMultiple) => {
+                http.fetch(`http://localhost:${port}/bricks/downloadMultipleBricks/${domain}/?hashes=${brickHash}`).then((responseMultiple) => {
                     assert.true(typeof responseMultiple.ok === 'boolean');
                     assert.true(responseMultiple.ok === true);
 
