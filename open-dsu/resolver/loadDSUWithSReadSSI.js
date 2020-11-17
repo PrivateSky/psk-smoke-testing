7f7457458641fdf4d563bae03a8282d004cb6d56
skutner <sava.dumitru.daniel@gmail.com>
2020-11-17 09:41:47 +0200

    added DSU factory for SReadSSI

---
+++ open-dsu/resolver/loadDSUWithSReadSSI.js
@@ -0,0 +1,57 @@
+require("../../../../psknode/bundles/testsRuntime");
+require("../../../../psknode/bundles/openDSU");
+
+const tir = require("../../../../psknode/tests/util/tir");
+const assert = require("double-check").assert;
+const openDSU = require("opendsu");
+const resolver = openDSU.loadApi("resolver");
+assert.callback("Create and load DSU test", (callback) => {
+    tir.launchVirtualMQNode((err, port) => {
+        if (err) {
+            throw err;
+        }
+
+        const bdns = openDSU.loadApi("bdns");
+        const keyssi = openDSU.loadApi("keyssi");
+        const seedSSI = keyssi.buildSeedSSI("localDomain", undefined, undefined, "v0", "hint");
+        bdns.addRawInfo("localDomain", {
+            brickStorages: [`http://localhost:${port}`],
+            anchoringServices: [`http://localhost:${port}`]
+        })
+
+        resolver.createDSU(seedSSI, (err, rawDossier) => {
+            if (err) {
+                throw err;
+            }
+
+            rawDossier.writeFile("/a.txt", "some data", (err) => {
+                if (err) {
+                    throw err;
+                }
+
+                rawDossier.getKeySSI("sread", (err, sreadSSI) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    resolver.loadDSU(sreadSSI, (err, loadedDSU) => {
+                        if (err) {
+                            throw err;
+                        }
+
+
+                        loadedDSU.getKeySSI((err, cloneKeySSI) => {
+                            if (err) {
+                                throw err;
+                            }
+
+                            assert.true(sreadSSI === cloneKeySSI);
+                            callback();
+                        });
+                    });
+
+                });
+            });
+        });
+    });
+}, 15000);
