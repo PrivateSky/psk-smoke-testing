88b79fbfbebd930f06121abec092dd6d83b2b920
Vlad Balmos <vlad.balmos@code932.com>
2020-12-03 16:46:34 +0200

    Add tests for listFolders recursively and delay anchoring

---
+++ edfs/bar/delayAnchoringTest.js
@@ -0,0 +1,63 @@
+require('../../../../psknode/bundles/testsRuntime');
+require("../../../../psknode/bundles/pskWebServer");
+
+const double_check = require("double-check");
+const assert = double_check.assert;
+
+const tir = require("../../../../psknode/tests/util/tir.js");
+const openDSU = require("opendsu");
+const resolver = openDSU.loadApi("resolver");
+const keySSISpace = openDSU.loadApi("keyssi");
+const bdns = openDSU.loadApi("bdns");
+
+double_check.createTestFolder("delay_anchoring_test_folder", (err, testFolder) => {
+    assert.true(err === null || typeof err === "undefined", "Failed to create test folder");
+
+    const fileData = "Lorem Ipsum is simply dummy text";
+
+    assert.callback("DelayAnchoringTest", (callback) => {
+        tir.launchVirtualMQNode(10, testFolder, (err, port) => {
+            assert.true(err === null || typeof err === "undefined", "Failed to create server");
+
+            const options = {
+                anchoringOptions: {
+                    anchoringEventListener: (err, result) => {
+                        callback(err);
+                    },
+                    delay: 500
+                }
+            }
+            resolver.createDSU(keySSISpace.buildSeedSSI("default"), options, (err, bar) => {
+                if (err) {
+                    throw err;
+                }
+
+                bar.writeFile("a.txt", fileData, (err, brickMapDigest) => {
+                    if (err) {
+                        throw err;
+                    }
+                    assert.true(err === null || typeof err === "undefined", "Failed to write file in BAR");
+
+                    //bar.getKeySSI((err, keySSI) => {
+                        //if (err) {
+                            //throw err;
+                        //}
+                        //resolver.loadDSU(keySSI, (err, newBar) => {
+                            //if (err) {
+                                //throw err;
+                            //}
+                            //newBar.readFile("a.txt", (err, data) => {
+                                //assert.true(err === null || typeof err === "undefined", "Failed read file from BAR.");
+                                //assert.true(fileData === data.toString(), "Invalid read data");
+
+                                //callback();
+                            //});
+                        //})
+                    //})
+                });
+            })
+        });
+    }, 2000);
+});
+
+
---
+++ edfs/bar/listFoldersTest.js
@@ -0,0 +1,166 @@
+require('../../../../psknode/bundles/testsRuntime');
+
+const double_check = require("double-check");
+const assert = double_check.assert;
+
+let folderPath;
+let filePath;
+
+let files;
+
+const tir = require("../../../../psknode/tests/util/tir.js");
+const openDSU = require("opendsu");
+const resolver = openDSU.loadApi("resolver");
+const keySSISpace = openDSU.loadApi("keyssi");
+const bdns = openDSU.loadApi("bdns");
+const text = ["first", "second", "third"];
+
+require("callflow").initialise();
+
+$$.flows.describe("ListFoldersTest", {
+    start: function (callback) {
+        this.callback = callback;
+
+        double_check.ensureFilesExist([folderPath], files, text, (err) => {
+            assert.true(err === null || typeof err === "undefined", "Failed to create folder hierarchy.");
+
+            tir.launchVirtualMQNode((err, port) => {
+                assert.true(err === null || typeof err === "undefined", "Failed to create server.");
+
+                this.createDSU((dsu) => {
+                    dsu.batch((done) => {
+                        dsu.addFolder(folderPath, 'fld1', (err) => {
+                            if (err) {
+                                return done(err);
+                            }
+
+                            dsu.addFolder(folderPath, 'fld1/fld2', (err) => {
+                                if (err) {
+                                    return done(err);
+                                }
+                                done();
+                            })
+                        });
+                    }, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+
+                        this.testListingFoldersTopLevel(dsu)
+                    })
+                });
+            });
+        });
+    },
+
+    createDSU: function (callback) {
+        $$.securityContext.generateIdentity((err, agentId) => {
+            assert.true(err === null || typeof err === "undefined", "Failed to generate identity.");
+            resolver.createDSU(keySSISpace.buildSeedSSI("default"), (err, dsu) => {
+                if (err) {
+                    throw err;
+                }
+
+                callback(dsu);
+            });
+        });
+    },
+
+    testListingFoldersTopLevel: function (dsu) {
+        dsu.listFolders('/', {recursive: true}, (err, folders) => {
+            if (err) {
+                throw err;
+            }
+
+            assert.true(folders.indexOf('fld1') !== -1, 'Folder not in dsu')
+            assert.true(folders.indexOf('fld1/fld2') !== -1, 'Subfolder not in dsu')
+
+            this.mountDSUs(dsu)
+        })
+    },
+
+    mountDSUs: function (dsu) {
+        this.createDSU((level1DSU) => {
+            level1DSU.addFolder(folderPath, 'fld', (err) => {
+                if (err) {
+                    throw err;
+                }
+
+                this.createDSU((level2DSU) => {
+                    level2DSU.addFolder(folderPath, 'fld', (err) => {
+                        if (err) {
+                            throw err;
+                        }
+
+                        level2DSU.getKeySSI((err, identifier) => {
+                            if (err) {
+                                throw err;
+                            }
+
+                            level1DSU.mount('/level2', identifier, (err) => {
+                                if (err) {
+                                    throw err;
+                                }
+
+                                level1DSU.getKeySSI((err, identifier) => {
+                                    if (err) {
+                                        throw err;
+                                    }
+
+                                    dsu.mount('/level1', identifier, (err) => {
+                                        if (err) {
+                                            throw err;
+                                        }
+
+                                        dsu.getKeySSI((err, identifier) => {
+                                            if (err) {
+                                                throw err;
+                                            }
+
+                                            this.testListingFoldersFromMountedDSUs(identifier)
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+
+                });
+            })
+        })
+
+    },
+
+    testListingFoldersFromMountedDSUs: function (keySSI) {
+        resolver.loadDSU(keySSI, (err, dsu) => {
+            if (err) {
+                throw err;
+            }
+
+            dsu.listFolders('/', {recursive: true}, (err, folders) => {
+                if (err) {
+                    throw err;
+                }
+
+
+                assert.true(folders.indexOf('fld1') !== -1, 'Folder not listed');
+                assert.true(folders.indexOf('fld1/fld2') !== -1, 'Folder not listed recursively');
+                assert.true(folders.indexOf('level1/level2/fld') !== -1, 'Level 2 mounted folders not listed')
+                assert.true(folders.indexOf('level1/fld') !== -1, 'Level 1 mounted folders not listed')
+
+                this.callback();
+            });
+        })
+    },
+});
+
+double_check.createTestFolder("list_files_test_folder", (err, testFolder) => {
+    const path = require("path");
+    folderPath = path.join(testFolder, "fld");
+    files = ["fld/a.txt", "fld/b.txt", "fld/c.txt"].map(file => path.join(testFolder, file));
+    filePath = path.join(testFolder, "test.txt");
+    assert.callback("List folders", (callback) => {
+        $$.flows.start("ListFoldersTest", "start", callback);
+    //}, 3000);
+    }, 3000 * 1000);
+});
