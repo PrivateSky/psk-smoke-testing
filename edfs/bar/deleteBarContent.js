1eb1e051d0bd9c928bf9ec12378cfce6a170a127
mastaleru <mastaleru.rafael@gmail.com>
2021-06-24 11:50:18 +0300

    added delete bar content test

---
+++ edfs/bar/deleteBarContent.js
@@ -0,0 +1,65 @@
+require('../../../../psknode/bundles/testsRuntime');
+require("../../../../psknode/bundles/pskWebServer");
+
+const double_check = require("double-check");
+const assert = double_check.assert;
+
+const tir = require("../../../../psknode/tests/util/tir.js");
+
+double_check.createTestFolder("bar_delete_content", (err, testFolder) => {
+    assert.true(err === null || typeof err === "undefined", "Failed to create test folder");
+
+    const fileData = "Lorem Ipsum is simply dummy text";
+
+    assert.callback("BasicBarFunctionality", (callback) => {
+        tir.launchVirtualMQNode(10, testFolder, (err, port) => {
+            assert.true(err === null || typeof err === "undefined", "Failed to create server");
+
+            const openDSU = require("opendsu");
+            const resolver = openDSU.loadApi("resolver");
+            const keySSISpace = openDSU.loadApi("keyssi");
+
+            resolver.createDSU(keySSISpace.createTemplateSeedSSI("default"), (err, bar) => {
+                if (err) {
+                    throw err;
+                }
+
+
+                // bar.delete("/", (err) => {
+                //     if (err) {
+                //         console.log("Empty bar delete");
+                //         throw err;
+                //     }
+                //     console.log("Finished empty bar delete")
+                //     assert.equal(err, undefined);
+                //
+                // })
+
+
+                bar.writeFile("a.txt", fileData, (err, brickMapDigest) => {
+                    console.log("Written data");
+
+                    bar.delete("/", (err) => {
+                        if(err){
+                            console.log("First bar delete");
+                            throw err;
+                        }
+                        console.log("Before second deletion")
+                        bar.delete("/", (err) => {
+
+                            if(err){
+                                throw err;
+                            }
+                        })
+                    })
+                })
+
+
+
+
+            })
+        });
+    }, 200000000000);
+});
+
+
